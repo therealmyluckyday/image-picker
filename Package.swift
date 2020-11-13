d817362806e79b3037dbe0b69024b6f0d570baf7
Joshua Pierce <jpierce@myluckyday.fr>
2020-11-13 14:59:49 +0100

    added SPM

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "ImagePicker",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "ImagePicker",
+            targets: [
+                "ImagePicker"
+            ]
+        )
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        .target(
+            name: "ImagePicker",
+            dependencies: [],
+            path: "ImagePicker"
+        )
+    ]
+)
